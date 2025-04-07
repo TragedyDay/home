30824170b05047967fa04a9574fcafa5a375be0d
TragedyDay <145722566+TragedyDay@users.noreply.github.com>
2025-04-07 17:24:10 +0800

    Update mc.js

--- mc.js
+++ mc.js
@@ -1,8 +1,8 @@
 async function checkServerStatus() {
     const serverIp = '47.120.67.232';
-    const serverPort = '100';
+    const serverPort = '1100';
 try {
-    const response = await fetch(`https://mcapi.us/server/status?ip=${'47.120.67.232'}&port=${'100'}`);
+    const response = await fetch(`https://mcapi.us/server/status?ip=${'47.120.67.232'}&port=${'1100'}`);
     const data = await response.json();
 
     const statusElement = document.getElementById('status');
